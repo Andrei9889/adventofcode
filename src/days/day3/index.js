bf55077742b3d54fe00c28f5a0d875ccaa9874db
Anton Abyzov <anton.abyzov@gmail.com>
2019-12-27 09:57:52 +0300

    day3-1 wip

--- src/days/day3/index.js
+++ src/days/day3/index.js
@@ -1,9 +1,6 @@
-import _ from 'lodash'
-
-
 export function day3() {
-  const firstLine = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
-  const secondLine = ['U62','R66','U55','R34','D71','R55','D58','R83']
+  const firstLine = ['T5', 'R3', 'D4']
+  const secondLine = ['R2', 'T4', 'R3']
 
   const getCoordinates = (line) => {
     let x = 0
@@ -11,27 +8,84 @@ export function day3() {
     let result = []
     line.forEach((step)=>{
       switch(step[0]){
-        case 'U':
-            x += +step.slice(1)
-          break
-        case 'R':
+        case 'T':
             y += +step.slice(1)
           break
+        case 'R':
+            x += +step.slice(1)
+          break
         case 'D':
-            x -= +step.slice(1)
+            y -= +step.slice(1)
           break
         case 'L':
-            y -= +step.slice(1)
+            x -= +step.slice(1)
           break
         default: return null 
       }
-
       result.push([x,y])
     })
     return result
   }  
+  
 
-  console.log(getCoordinates(firstLine))
+  let intersection = (X, Y) => {
+    let a1 = X[0][1] - X[1][1]
+    let b1 = X[1][0] - X[0][0]
+    let a2 = Y[0][1] - Y[1][1]
+    let b2 = Y[1][0] - Y[0][0]
 
-  console.log(_.intersection([1,2], [4,2]))
+    let d = a1 * b2 - a2 * b1
+
+    console.log(a1, b1, a2, b2, d)
+
+    if(d !== 0){
+      let c1 = X[1][1] * X[0][0] - X[1][0] * X[0][1]
+      let c2 = Y[1][1] * Y[0][0] - Y[1][0] * Y[0][1]
+
+      
+      let xi = (b1 * c2 - b2 * c1) / d
+      let yi = (a2 * c1 - a1 * c2) / d;
+      return [xi, yi]
+    }
+  }
+  
+  let red = getCoordinates(firstLine)
+  let blue = getCoordinates(secondLine)
+
+  console.log(intersection([[3,5], [3,1]], [[0,0],[2,0]]))
+
+
+  // let arrayOfIntersections = []
+
+  // blue.forEach((blueElem, blueIndex)=>{
+
+  //   let blueEnd = blueElem
+  //   let blueStart = blueIndex === 0 ? [0,0] : blue[blueIndex - 1]
+
+  //   red.forEach((redElem, redIndex)=>{
+  //     let redEnd = redElem
+  //     let redStart = redIndex === 0 ? [0,0] : red[redIndex - 1]
+
+  //     console.log([redStart, redEnd], [blueStart, blueEnd])
+
+  //     let defined = intersection([redStart, redEnd], [blueStart, blueEnd])
+  //     console.log(defined)
+  //     // if(defined){
+  //     //   arrayOfIntersections.push(defined)
+  //     // }
+
+  //     console.log('---------------------------')
+  //   })
+  // })
+
+  // console.log(arrayOfIntersections)
+  // let arrayOfDistances = arrayOfIntersections.map((coordinates)=>{
+  //   return coordinates[0] + coordinates[1]
+  // })
+
+  // arrayOfDistances.sort()
+
+  // console.log(arrayOfDistances)
+  
+
 }
